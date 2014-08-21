82325f7281cab5b26e1172899ab12453d7afbd5f
daichi <prinum101@gmail.com>
2014-08-21 22:03:34 +0900

    変数を渡さない

--- unity-server-master/routes/index.js
+++ unity-server-master/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   // res.render('index', { title: '' });
-  res.render('index', { title: 'User' });
+  console.log('hi');
+  res.render('index');
 });
 
 module.exports = router;
