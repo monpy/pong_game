5c73a7efa31fec8d27f55b6801c0daf02cfd37d3
monpy <4696monpy@gmail.com>
2014-08-21 21:29:56 +0900

    oraoraoraor

--- unity-server-master/public/javascripts/app.js
+++ unity-server-master/public/javascripts/app.js
@@ -3,7 +3,33 @@ $(function(){
   var $down_button = $('#down_button');
   var $notice = $('#notice');
   var $enemy = $('#enemy');
+  var $controller = $('#controller');
   var socket = io.connect();
+  var nowY = 0;
+  var h = $(window).height();
+
+  var mc = new Hammer(document.getElementById('controller'));
+  mc.on("pan panstart panmove", function(ev) {
+    nowY = ev.center.y;
+  });
+
+  mc.on("panend", function(ev) {
+    nowY = 0;
+  });
+
+  var gameTimer = setInterval(function(){
+    if (nowY == 0) {
+      return;
+    }
+
+    if (nowY < h / 2){
+      console.log('up');
+      socket.emit('up');
+    }else{
+      console.log('down');
+      socket.emit('down');
+    }
+  },1000/60);
 
   $up_button.on('click', function() {
     console.log('up');
@@ -16,7 +42,9 @@ $(function(){
   });
 
   socket.on('connect', function() {
+    $notice.show();
     $notice.html('サーバーと接続しました');
+    $notice.delay(300).fadeOut('300', function() {});
   });
 
   socket.on('enemy', function(mes) {
@@ -25,7 +53,9 @@ $(function(){
   });
 
   socket.on('newPlayer', function() {
+    $notice.show();
     $notice.html('新しい敵が入りました');
+    $notice.fadeOut('300', function() {});
   });
 
   socket.on('otherDisconnect', function() {
